d8436b6e8f014d3f5ce72edb744121a7c44d265a
Richard Parsons <parsons.rk@gmail.com>
2019-05-14 16:34:14 +0200

    firebase config

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,15 +1,16 @@
 const config = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
 }
 
 class Firebase {
     constructor(app) {
-        console.log('API_KEY', process.env.REACT_APP_API_KEY)
+        console.log('API_KEY', process.env.API_KEY)
+        console.log('GATSBY_API_KEY', process.env.GATSBY_API_KEY)
         app.initializeApp(config)
 
         /* Helper */
@@ -50,7 +51,7 @@ class Firebase {
 
     doSendEmailVerification = () =>
         this.auth.currentUser.sendEmailVerification({
-            url: process.env.REACT_APP_CONFIRMATION_EMAIL_REDIRECT,
+            url: process.env.CONFIRMATION_EMAIL_REDIRECT,
         })
 
     doPasswordUpdate = password =>
