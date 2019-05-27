d0b56a5828cef7c9036fed967762d85ef48e694d
Richard Parsons <parsons.rk@gmail.com>
2019-05-27 19:12:40 +0200

    lambda firebase auth

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,48 +1,36 @@
 const admin = require('firebase-admin')
 
-console.log(
-    'service account encoded: ',
-    process.env.FIREBASE_SERVICE_ACCOUNT_BASE64
-)
-
-// admin.initializeApp({
-//     credential: admin.credential.cert(
-//         JSON.parse(
-//             new Buffer(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, 'base64')
-//         )
-//     ),
-//     databaseURL: 'https://my-firebase-app.firebaseio.com',
-// })
+admin.initializeApp({
+    credential: admin.credential.cert(
+        JSON.parse(
+            Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, 'base64')
+        )
+    ),
+    databaseURL: 'https://my-firebase-app.firebaseio.com',
+})
 
 export function handler(event, context, callback) {
     var idToken = event.headers.authorization.split('Bearer ')[1]
 
-    callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({
-            msg: 'ok',
-        }),
-    })
-
-    // admin
-    //     .auth()
-    //     .verifyIdToken(idToken)
-    //     .then(function(decodedToken) {
-    //         console.log(`Auth success - uid=${decodedToken.uid}`)
-    //         callback(null, {
-    //             statusCode: 200,
-    //             body: JSON.stringify({
-    //                 msg: 'Hello, World! ' + Math.round(Math.random() * 10),
-    //             }),
-    //         })
-    //     })
-    //     .catch(function(error) {
-    //         console.log(`Auth fail - "${error}"`)
-    //         callback(null, {
-    //             statusCode: 401,
-    //             body: JSON.stringify({
-    //                 msg: 'Unauthorized user',
-    //             }),
-    //         })
-    //     })
+    admin
+        .auth()
+        .verifyIdToken(idToken)
+        .then(function(decodedToken) {
+            console.log(`Auth success - uid=${decodedToken.uid}`)
+            callback(null, {
+                statusCode: 200,
+                body: JSON.stringify({
+                    msg: 'Hello, World! ' + Math.round(Math.random() * 10),
+                }),
+            })
+        })
+        .catch(function(error) {
+            console.log(`Auth fail - "${error}"`)
+            callback(null, {
+                statusCode: 401,
+                body: JSON.stringify({
+                    msg: 'Unauthorized user',
+                }),
+            })
+        })
 }
