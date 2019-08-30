c3fce88b8eab258b274c895a192acd0b7f34fd7e
Richard Parsons <parsons.rk@gmail.com>
2019-08-30 10:59:03 +0200

    added urls

--- src/components/DashboardItem/index.js
+++ src/components/DashboardItem/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, graphql } from 'gatsby'
+import { Link } from 'gatsby'
 import styles from './index.module.scss'
 import { Flipped } from 'react-flip-toolkit'
 
