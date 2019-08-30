812d8581281dbb3534c0df24a3faa01c243f81a1
Richard Parsons <parsons.rk@gmail.com>
2019-08-30 10:52:27 +0200

    added url to query

--- src/pages/artists.js
+++ src/pages/artists.js
@@ -16,6 +16,7 @@ export const query = graphql`
             edges {
                 node {
                     id
+                    url
                     title
                     thumbnail
                 }
