3e7213f7b9f3fc4d31847fc7e851e3e3004e5074
Richard Parsons <parsons.rk@gmail.com>
2019-09-06 14:48:57 +0200

    fixed urls

--- src/components/DashboardItem/index.js
+++ src/components/DashboardItem/index.js
@@ -5,7 +5,7 @@ import { Flipped } from 'react-flip-toolkit'
 
 const DashboardItem = ({ fields, title, thumbnail }) => (
     <Flipped flipId={fields.id}>
-        <Link to={fields.url}>
+        <Link to={`/${fields.url}`}>
             <img src={thumbnail} alt={title} className={styles.thumbnail} />
             <h4>{title}</h4>
         </Link>
