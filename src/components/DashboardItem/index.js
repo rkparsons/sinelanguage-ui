da6e608e25c4da138d27d87bb4322165d45158d6
Richard Parsons <parsons.rk@gmail.com>
2019-09-06 14:35:22 +0200

    head tags

--- src/components/DashboardItem/index.js
+++ src/components/DashboardItem/index.js
@@ -3,7 +3,7 @@ import { Link } from 'gatsby'
 import styles from './index.module.scss'
 import { Flipped } from 'react-flip-toolkit'
 
-const DashboardItem = ({ fields, title, thumbnail }) => (
+const DashboardItem = id => (
     <Flipped flipId={fields.id}>
         <Link to={fields.url}>
             <img src={thumbnail} alt={title} className={styles.thumbnail} />
