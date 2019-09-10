5a8addec98c27d0fcafb3636763632ffffa7ae55
Rick Parsons <parsons.rk@gmail.com>
2019-09-10 16:51:21 +0200

    enable login widget

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
         {
             resolve: `gatsby-plugin-netlify-cms`,
             options: {
-                enableIdentityWidget: false,
+                enableIdentityWidget: true,
                 modulePath: `${__dirname}/src/cms/cms.js`,
                 manualInit: true,
             },
