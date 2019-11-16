2d8caea65742cdf450a9e1164c0c3b5959435cea
Richard Parsons <parsons.rk@gmail.com>
2019-11-16 11:58:01 +0100

    seo formatting

--- src/cms/partials/seo.js
+++ src/cms/partials/seo.js
@@ -1,17 +1,17 @@
 export const seo = [
     {
-      label: 'SEO Title',
-      name: 'seo_title',
-      widget: 'string',
+        label: 'SEO Title',
+        name: 'seo_title',
+        widget: 'string',
     },
     {
-      label: 'Tagline',
-      name: 'seo_description',
-      widget: 'string',
+        label: 'Tagline',
+        name: 'seo_description',
+        widget: 'string',
     },
     {
-      label: 'Image (rect)',
-      name: 'seo_image',
-      widget: 'image',
+        label: 'Image (rect)',
+        name: 'seo_image',
+        widget: 'image',
     },
-  ];
+]
