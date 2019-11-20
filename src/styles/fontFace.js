c426a02998b9667a5ca9dff0d9542390b02859d2
Richard Parsons <parsons.rk@gmail.com>
2019-11-20 09:41:18 +0100

    refactored theme

---
+++ src/styles/fontFace.js
@@ -0,0 +1,14 @@
+import robotoWoff from '../fonts/Roboto.woff'
+import robotoWoff2 from '../fonts/Roboto.woff2'
+
+export default {
+    fontFamily: 'Roboto',
+    fontDisplay: 'block',
+    src: `
+    local('Roboto'),
+    local('Roboto-Regular'),
+    url(${robotoWoff2}) format('woff2'),
+    url(${robotoWoff}) format('woff')
+  `,
+    unicodeRange: 'U+000-5FF',
+}
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,30 +1,7 @@
 import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles'
 
-import indigo from '@material-ui/core/colors/indigo'
-import pink from '@material-ui/core/colors/pink'
-import red from '@material-ui/core/colors/red'
-import robotoWoff from '../fonts/Roboto.woff'
-import robotoWoff2 from '../fonts/Roboto.woff2'
-
-const robotoFontFace = {
-    fontFamily: 'Roboto',
-    fontDisplay: 'block',
-    src: `
-    local('Roboto'),
-    local('Roboto-Regular'),
-    url(${robotoWoff2}) format('woff2'),
-    url(${robotoWoff}) format('woff')
-  `,
-    unicodeRange: 'U+000-5FF',
-}
-
-const palette = {
-    primary: indigo,
-    secondary: pink,
-    error: red,
-    contrastThreshold: 3,
-    tonalOffset: 0.2,
-}
+import fontFace from './fontFace'
+import palette from './palette'
 
 export default (isDarkMode = false) =>
     responsiveFontSizes(
@@ -36,7 +13,7 @@ export default (isDarkMode = false) =>
             overrides: {
                 MuiCssBaseline: {
                     '@global': {
-                        '@font-face': [robotoFontFace],
+                        '@font-face': [fontFace],
                     },
                 },
             },
