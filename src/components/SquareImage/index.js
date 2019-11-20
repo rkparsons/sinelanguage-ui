e02c4f5e67153d580a3d9f1ff31ac95724ad8b51
Richard Parsons <parsons.rk@gmail.com>
2019-11-20 12:34:53 +0100

    comment

--- src/components/SquareImage/index.js
+++ src/components/SquareImage/index.js
@@ -2,5 +2,6 @@ import React from 'react'
 import styles from './index.module.scss'
 
 export default ({ image }) => (
+    // replace with gatsby image if flickering issue fixed
     <img src={image.localFile.childImageSharp.fluid.src} className={styles.thumbnail} />
 )
