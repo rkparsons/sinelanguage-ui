0aa10b7a4af4525ef2373cdcfb8ee72a8db03993
Rick Parsons <parsons.rk@gmail.com>
2019-11-21 13:50:32 +0100

    split mailchimp

--- src/components/Footer/Footer.view.tsx
+++ src/components/Footer/Footer.view.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 import Switch from '@material-ui/core/Switch'
 import { muiSwitchColour } from './Footer.style'
 
-interface FooterProps {
+interface Props {
     isDarkMode: boolean
     setIsDarkMode: (isDarkMode: boolean) => void
 }
 
-export default ({ isDarkMode, setIsDarkMode }: FooterProps) => {
+export default ({ isDarkMode, setIsDarkMode }: Props) => {
     return (
         <footer>
             <MailChimp />
