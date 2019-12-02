c8b65be1ae022d178774358baeeef950f4250382
Richard Parsons <parsons.rk@gmail.com>
2019-12-02 14:25:29 +0100

    removed news type

--- src/hooks/useNewsQuery.ts
+++
@@ -1,9 +0,0 @@
-import { graphql } from 'gatsby'
-import { useStaticQuery } from 'gatsby'
-
-export default () =>
-    useStaticQuery(graphql`
-        query News {
-            ...newsFragment
-        }
-    `)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,8 @@
 import Dashboard from '~/components/Dashboard'
 import React from 'react'
 import { graphql } from 'gatsby'
-import useNewsQuery from '~/hooks/useNewsQuery'
 
 export default ({ data }) => {
-    const news = useNewsQuery()
-    console.log(news)
-
     return <Dashboard {...data} />
 }
 
