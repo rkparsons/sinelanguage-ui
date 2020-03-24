58c59a401e99fe32c478718c6ff57e44101a36ce
Richard Parsons <parsons.rk@gmail.com>
2020-03-24 15:49:08 +0100

    Remove unused import

--- gatsby/index.js
+++ gatsby/index.js
@@ -1,12 +1,10 @@
 require('source-map-support').install()
 require('ts-node').register()
 
-const { browserAPI } = require('./browserApi')
 const { configAPI } = require('./configApi')
 const { nodeAPI } = require('./nodeApi')
 
 module.exports = {
-    browserAPI,
     configAPI,
     nodeAPI,
 }
