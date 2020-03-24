e1f058c16996e83b350b301da0207a4d3e1a5041
Richard Parsons <parsons.rk@gmail.com>
2020-03-24 18:55:11 +0100

    Update header

--- src/components/Navigation/Navigation.view.tsx
+++ src/components/Navigation/Navigation.view.tsx
@@ -7,42 +7,46 @@ import { NavList } from './Navigation.style'
 import React from 'react'
 import SignInButton from '~/components/SignInButton'
 import SignOutButton from '~/components/SignOutButton'
+import { Typography } from '@material-ui/core'
 import { isAuthenticated } from '~/utils/auth'
 
 export default () => (
-    <NavList>
-        <li>
-            <NavItem to={ROUTES.NEWS} title="News" />
-        </li>
-        <li>
-            <NavItem to={ROUTES.ARTISTS} title="Artists" partiallyActive={true} />
-        </li>
-        <li>
-            <NavItem to={ROUTES.RELEASES} title="Releases" partiallyActive={true} />
-        </li>
-        <li>
-            <NavItem to={ROUTES.PODCASTS} title="Podcasts" partiallyActive={true} />
-        </li>
-        <li>
-            <NavItem to={ROUTES.EVENTS} title="Events" partiallyActive={true} />
-        </li>
-        <li>
-            <NavItem to={ROUTES.CONTACT} title="Contact" partiallyActive={true} />
-        </li>
-        {isAuthenticated() ? (
-            <>
-                <li>
-                    <NavItem to={ROUTES.ACCOUNT} title="Account" partiallyActive={true} />
-                </li>
-                <li>
-                    <SignOutButton />
-                </li>
-            </>
-        ) : (
+    <>
+        <Typography>SINE LANGUAGE RECORDS</Typography>
+        <NavList>
             <li>
-                <SignInButton />
-                <MenuIcon />
+                <NavItem to={ROUTES.NEWS} title="News" />
             </li>
-        )}
-    </NavList>
+            <li>
+                <NavItem to={ROUTES.ARTISTS} title="Artists" partiallyActive={true} />
+            </li>
+            <li>
+                <NavItem to={ROUTES.RELEASES} title="Releases" partiallyActive={true} />
+            </li>
+            <li>
+                <NavItem to={ROUTES.PODCASTS} title="Podcasts" partiallyActive={true} />
+            </li>
+            <li>
+                <NavItem to={ROUTES.EVENTS} title="Events" partiallyActive={true} />
+            </li>
+            <li>
+                <NavItem to={ROUTES.CONTACT} title="Contact" partiallyActive={true} />
+            </li>
+            {/* {isAuthenticated() ? (
+                <>
+                    <li>
+                        <NavItem to={ROUTES.ACCOUNT} title="Account" partiallyActive={true} />
+                    </li>
+                    <li>
+                        <SignOutButton />
+                    </li>
+                </>
+            ) : (
+                <li>
+                    <SignInButton />
+                    <MenuIcon />
+                </li>
+            )} */}
+        </NavList>
+    </>
 )
