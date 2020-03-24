f6d9f2c94a06417e2d06b16866c82b363c3d899f
Richard Parsons <parsons.rk@gmail.com>
2020-03-24 15:44:23 +0100

    Refactor gatsby browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,34 +1,3 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import { initAnalytics, isAnalyticsEnabled, trackPageView } from '~/utils/cookies'
+const { browserAPI } = require('./gatsby/browserApi')
 
-import { silentAuth } from '~/utils/auth'
-
-export const onClientEntry = () => {
-    if (isAnalyticsEnabled()) {
-        initAnalytics()
-    }
-}
-
-export const onRouteUpdate = (state, page, pages) => {
-    if (isAnalyticsEnabled()) {
-        trackPageView(state.location.pathname)
-    }
-}
-
-const SessionCheck = ({ children }) => {
-    const [isLoading, setIsLoading] = useState(true)
-
-    const handleCheckSession = useCallback(() => {
-        setIsLoading(false)
-    }, [setIsLoading])
-
-    useEffect(() => {
-        silentAuth(handleCheckSession)
-    }, [handleCheckSession])
-
-    return <>{!isLoading && children}</>
-}
-
-export const wrapRootElement = ({ element }) => {
-    return <SessionCheck>{element}</SessionCheck>
-}
+module.exports = browserAPI
--- gatsby/index.js
+++ gatsby/index.js
@@ -1,12 +1,12 @@
 require('source-map-support').install()
 require('ts-node').register()
 
+const { browserAPI } = require('./browserApi')
 const { configAPI } = require('./configApi')
 const { nodeAPI } = require('./nodeApi')
-// const { browserAPI } = require('./browserApi')
 
 module.exports = {
+    browserAPI,
     configAPI,
     nodeAPI,
-    // browserAPI,
 }
