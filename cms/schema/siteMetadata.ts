5496fead9866bd535c80fb1c3725aef3fc5e2563
Richard Parsons <parsons.rk@gmail.com>
2020-04-07 16:56:27 +0200

    Add sitemetadata schema type

--- cms/schema/siteMetadata.ts
+++ cms/schema/siteMetadata.ts
@@ -1,82 +1,47 @@
-import { assetFileSize } from '../validations/assetFileSize'
+import { ImageField, SymbolField, TextField } from '../models'
+import { assetFileSize, unique } from '../validations'
+
+import { Control } from '../types/control'
+
+const fields = [
+    new SymbolField({
+        name: 'Title',
+    }),
+    new SymbolField({
+        name: 'Url',
+        validations: [
+            unique,
+            {
+                in: ['https://sinelanguage.netlify.com', 'https://sinelanguage.net'],
+            },
+        ],
+    }),
+    new TextField({
+        name: 'Description',
+        validations: [
+            {
+                size: {
+                    min: 0,
+                    max: 256,
+                },
+                message: 'Description should be no more than 256 characters',
+            },
+        ],
+        widgetId: 'multipleLine',
+        helpText:
+            'A short description of Sine Language will be used when sharing on social media, and to improve SEO',
+    }),
+    new ImageField({
+        name: 'Image',
+        validations: [assetFileSize],
+    }),
+]
 
 export default {
     id: 'siteMetadata',
     name: 'Site Metadata',
     description: 'Metadata used for SEO and when linking to the site.',
     displayField: 'title',
-    fields: [
-        {
-            id: 'title',
-            name: 'Title',
-            type: 'Symbol',
-            localized: false,
-            required: true,
-            validations: [],
-            disabled: false,
-            omitted: false,
-        },
-        {
-            id: 'url',
-            name: 'Url',
-            type: 'Symbol',
-            localized: false,
-            required: true,
-            validations: [
-                {
-                    unique: true,
-                },
-                {
-                    in: ['https://sinelanguage.netlify.com', 'https://sinelanguage.net'],
-                },
-            ],
-            disabled: false,
-            omitted: false,
-        },
-        {
-            id: 'description',
-            name: 'Description',
-            type: 'Text',
-            localized: false,
-            required: true,
-            validations: [
-                {
-                    size: {
-                        min: 0,
-                        max: 256,
-                    },
-                    message: 'Description should be no more than 256 characters',
-                },
-            ],
-            disabled: false,
-            omitted: false,
-        },
-        {
-            id: 'image',
-            name: 'Image',
-            type: 'Link',
-            localized: false,
-            required: true,
-            validations: [
-                {
-                    linkMimetypeGroup: ['image'],
-                },
-                assetFileSize,
-            ],
-            disabled: false,
-            omitted: false,
-            linkType: 'Asset',
-        },
-    ],
-    controls: [
-        {
-            fieldId: 'description',
-            widgetNamespace: 'builtin',
-            widgetId: 'multipleLine',
-            settings: {
-                helpText:
-                    'A short description of Sine Language will be used when sharing on social media, and to improve SEO',
-            },
-        },
-    ],
+    fields: fields,
+    controls: fields.map(x => x.control).filter(control => control !== undefined) as Control[],
 }
