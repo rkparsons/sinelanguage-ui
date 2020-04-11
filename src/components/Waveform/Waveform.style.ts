ea5ee6e7a38ea6c6053bdb7a22781cafdc4248cc
Richard Parsons <parsons.rk@gmail.com>
2020-04-11 18:14:00 +0200

    Refactor waveform styling

--- src/components/Waveform/Waveform.style.ts
+++ src/components/Waveform/Waveform.style.ts
@@ -1,41 +1,45 @@
-import { Box, BoxProps, Icon, Theme } from '@material-ui/core'
+import { Box, Theme } from '@material-ui/core'
 
 import styled from 'styled-components'
 
+type PlayerProps = {
+    theme: Theme
+}
+
 export const Player = styled(Box)`
-    .react-waves {
-        width: 100%;
-        padding: 0;
-        position: relative;
-        cursor: pointer;
-        pointer-events: all;
-    }
-`
-
-export const WaveContainer = styled(Box)`
-    ${({ theme }: { theme: Theme }) => `        
-        position: relative;
-        &:after {
-            content: ' ';
-            background-color: ${theme.palette.background.default};
-            opacity: 0.5;
-            position: absolute;
-            display: block;
+    ${({ theme }: PlayerProps) => `      
+        .react-waves {
             width: 100%;
-            top: 0;
-            left: 0;
-            height: 100%;
-            border-radius: 10px;
-            z-index: 10;
-            pointer-events: none;
-            transition: opacity 0.1s linear;
-        }
+            padding: 0;
+            position: relative;
+            cursor: pointer;
+            pointer-events: all;
 
-        &:hover {
             &:after {
                 content: ' ';
-                opacity: 0;
+                background-color: ${theme.palette.background.default};
+                opacity: 0.5;
+                position: absolute;
+                display: block;
+                width: 100%;
+                top: 0;
+                left: 0;
+                height: 100%;
+                border-radius: 10px;
+                z-index: 10;
+                pointer-events: none;
+                transition: opacity 0.1s linear;
             }
-        }
+
+            &:hover {
+                &:after {
+                    content: ' ';
+                    opacity: 0;
+                }
+            }
+        }     
+        
     `}
 `
+
+export const WaveContainer = styled(Box)``
