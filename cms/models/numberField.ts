81ed3ceb0076bbc0ab3bc75e76b95a4989ce9161
Richard Parsons <parsons.rk@gmail.com>
2020-04-11 15:10:07 +0200

    Add number field to cms

---
+++ cms/models/numberField.ts
@@ -0,0 +1,41 @@
+import ContentfulField from './contentfulField'
+import { SubFieldProps } from '../types/subFieldProps'
+
+export default class NumberField extends ContentfulField {
+    constructor({
+        id,
+        name,
+        localized,
+        required,
+        validations,
+        disabled,
+        omitted,
+        widgetId,
+        helpText,
+        format,
+    }: SubFieldProps) {
+        super({
+            id,
+            name,
+            type: 'Integer',
+            localized,
+            required,
+            validations,
+            disabled,
+            omitted,
+            widgetId,
+            helpText,
+            format,
+        })
+    }
+
+    getTypeDefinitionImports = () => []
+
+    getTypeDefinition = () => `${this.contentFields.id}: number`
+
+    getLinkedNodeDefinition = () => undefined
+
+    getNodeDefinition = () => `${this.contentFields.id}: Int`
+
+    getFragmentDefinition = () => this.contentFields.id
+}
