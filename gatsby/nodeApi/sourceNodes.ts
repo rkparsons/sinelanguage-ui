b4f4215d5d727facb6ed1489ec716eca6b02d2b0
Richard Parsons <parsons.rk@gmail.com>
2020-04-12 18:37:20 +0200

    Refactor source nodes

--- gatsby/nodeApi/sourceNodes.ts
+++ gatsby/nodeApi/sourceNodes.ts
@@ -15,89 +15,17 @@ const createHash = (obj: object) =>
         .update(JSON.stringify(obj))
         .digest('hex')
 
-// todo: get rid of any types
-const processEntity = (entity: any) => ({
-    ...entity,
-    parent: '__SOURCE__',
-    children: [],
-    internal: {
-        type: internalType(entity.kind),
-        contentDigest: createHash(entity),
-    },
-})
-
-const collectTracksFromPlaylist = (accTracks: any, playlist: any) => [
-    ...accTracks,
-    ...playlist.tracks,
-]
-
-const addIfUnique = (uniqueItems: any, item: any) =>
-    uniqueItems.find((uniqueItem: any) => uniqueItem.id === item.id)
-        ? uniqueItems
-        : [...uniqueItems, item]
-
-const addReverseLink = (node: any, linkedNode: any) => {
-    if (linkedNode[`${node.kind}s___NODE`] === undefined) {
-        linkedNode[`${node.kind}s___NODE`] = []
-    }
-    linkedNode[`${node.kind}s___NODE`].push(node.id)
-}
-
 function fetchUserResource(resource: any, userID: number, clientID: string) {
     const url = `https://api.soundcloud.com/users/${userID}${resource}?client_id=${clientID}`
     return axios.get(url)
 }
 
 function linkNodes(nodes: any) {
-    return nodes
-        .map((node: any) => {
-            node.soundcloud_id = node.id
-            node.id = internalId(node.id, node.kind)
-            return node
-        })
-        .map((node: any) => {
-            let user: any = undefined
-
-            if (node.user) {
-                user = nodes.find(
-                    (user: any) => user.id === internalId(node.user.id, node.user.kind)
-                )
-
-                if (user) {
-                    // Add link to user node
-                    node.user___NODE = internalId(node.user.id, node.user.kind)
-                    delete node.user
-
-                    // Add reverse link to node on user
-                    addReverseLink(node, user)
-                }
-            }
-
-            if (node.tracks) {
-                // Add links to tracks on node
-                node.tracks___NODE = node.tracks.map((track: any) =>
-                    internalId(track.id, track.kind)
-                )
-
-                // Add reverse links to node on tracks and users
-                node.tracks.map((track: any) => {
-                    const matchingTrack = nodes.find(
-                        (user: any) => user.id === internalId(track.id, track.kind)
-                    )
-
-                    if (matchingTrack) {
-                        addReverseLink(node, matchingTrack)
-                    }
-                    if (user) {
-                        addReverseLink(node, user)
-                    }
-                })
-
-                delete node.tracks
-            }
-
-            return node
-        })
+    return nodes.map((node: any) => {
+        node.soundcloud_id = node.id
+        node.id = internalId(node.id, node.kind)
+        return node
+    })
 }
 
 export const sourceNodes: GatsbyNode['sourceNodes'] = async ({
@@ -121,7 +49,17 @@ export const sourceNodes: GatsbyNode['sourceNodes'] = async ({
         const entities = linkNodes([...tracks.data])
 
         // Process data into nodes.
-        entities.forEach((entity: any) => createNode(processEntity(entity)))
+        entities.forEach((entity: any) =>
+            createNode({
+                ...entity,
+                parent: '__SOURCE__',
+                children: [],
+                internal: {
+                    type: internalType(entity.kind),
+                    contentDigest: createHash(entity),
+                },
+            })
+        )
 
         // We're done, return.
         return
