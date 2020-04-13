adc53508a60848e8ce2d646c9d77112c4566246c
Richard Parsons <parsons.rk@gmail.com>
2020-04-13 17:07:28 +0200

    Remove layout padding

--- src/components/Layout/Layout.style.ts
+++ src/components/Layout/Layout.style.ts
@@ -8,6 +8,7 @@ export const GlobalStyle = createGlobalStyle`
 export const AppContainer = styled(Container)`
     min-height: 100vh;
     max-width: none;
+    padding: 0;
 `
 
 export const Main = styled.main``
