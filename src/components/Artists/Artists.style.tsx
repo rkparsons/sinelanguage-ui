d4d0db47c315b0f780c56e33a58f7f44a164efbf
Richard Parsons <parsons.rk@gmail.com>
2020-04-13 18:49:02 +0200

    Invert highlighted artist row

---
+++ src/components/Artists/Artists.style.tsx
@@ -0,0 +1,43 @@
+import { Box } from '@material-ui/core'
+import React from 'react'
+import styled from 'styled-components'
+
+export const Artists = styled(Box)`
+    ${({ theme }) => `
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 500;
+        padding-top: ${theme.spacing(20)};
+    `}
+`
+
+export const ArtistRow = styled(Box)`
+    cursor: pointer;
+    text-transform: uppercase;
+    &:hover {
+        background-color: rgba(255, 255, 255, 0);
+        backdrop-filter: invert(100%);
+    }
+`
+
+type FilterLayerProps = {
+    height: number
+    offset: number
+}
+
+export const FilterLayer = styled(({ height, offset, ...rest }: FilterLayerProps) => (
+    <Box {...rest} />
+))`
+    ${({ height, offset }: FilterLayerProps) => `
+        position: fixed;
+        z-index: 100;
+        width: 100%;
+        height: ${height}px;
+        margin-top: ${offset}px;
+        background-color: rgba(255, 255, 255, 0.15);
+        backdrop-filter: blur(10px);
+    `}
+`
--- src/components/Artists/Artists.view.tsx
+++ src/components/Artists/Artists.view.tsx
@@ -1,23 +1,60 @@
-import { ArtistRow, Artists } from './Artists.style'
-import { Box, Grid, Typography } from '@material-ui/core'
+import { ArtistRow, Artists, FilterLayer } from './Artists.style'
+import { Box, Grid, RootRef, Typography } from '@material-ui/core'
+import React, { ReactNode, useCallback, useRef, useState } from 'react'
 
 import { Artist } from '~/cms/types'
-import React from 'react'
 
 type ViewProps = {
     artists: Artist[]
 }
 
-export default ({ artists }: ViewProps) => (
-    <Artists>
-        <Grid container direction="column">
-            {artists.map(artist => (
-                <Grid item xs={12}>
-                    <ArtistRow>
-                        <Typography variant="h2">{artist.title}</Typography>
-                    </ArtistRow>
+export default ({ artists }: ViewProps) => {
+    const [verticalBreakpoints, setVerticalBreakpoints] = useState<number[]>([0, 0])
+    const rows = useRef<(Element | null)[]>([])
+
+    const highlightRow = useCallback(
+        (index?: number) => {
+            if (!index) {
+                setVerticalBreakpoints([0, 0])
+            }
+
+            const rowElement = rows.current[index!]
+
+            if (rowElement) {
+                const rowRect = rowElement.getBoundingClientRect()
+                setVerticalBreakpoints([rowRect.top, rowRect.bottom])
+            }
+        },
+        [setVerticalBreakpoints]
+    )
+
+    return (
+        <>
+            <FilterLayer height={verticalBreakpoints[0]} offset={0}></FilterLayer>
+            <Artists>
+                <Grid container direction="column">
+                    {artists.map((artist, index) => (
+                        <Grid item xs={12} key={index}>
+                            <RootRef
+                                rootRef={ref => {
+                                    rows.current.push(ref)
+                                }}
+                            >
+                                <ArtistRow
+                                    onMouseEnter={() => highlightRow(index)}
+                                    onMouseLeave={() => highlightRow()}
+                                >
+                                    <Typography variant="h2">{artist.title}</Typography>
+                                </ArtistRow>
+                            </RootRef>
+                        </Grid>
+                    ))}
                 </Grid>
-            ))}
-        </Grid>
-    </Artists>
-)
+            </Artists>
+            <FilterLayer
+                height={window.innerHeight - verticalBreakpoints[1]}
+                offset={verticalBreakpoints[1]}
+            ></FilterLayer>
+        </>
+    )
+}
