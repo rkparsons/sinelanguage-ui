8f84be74a56a8f77a8e9ed52541b657ef6b0df39
Richard Parsons <parsons.rk@gmail.com>
2020-04-19 13:37:41 +0200

    Increase focus image size

--- src/components/ContentList/ContentList.style.tsx
+++ src/components/ContentList/ContentList.style.tsx
@@ -36,7 +36,7 @@ export const Row = styled(Box)`
 `
 
 export const InvertBlurLayer = styled(Grid)`
-    width: ${100 / 3}vw;
+    width: ${30}vw;
     ${Row}:hover & {
         background-color: rgba(255, 255, 255, 0);
         backdrop-filter: blur(7px) invert(100%);
@@ -44,7 +44,7 @@ export const InvertBlurLayer = styled(Grid)`
 `
 
 export const InvertLayer = styled(Grid)`
-    width: ${100 / 3}vw;
+    width: ${40}vw;
     ${Row}:hover & {
         background-color: rgba(255, 255, 255, 0);
         backdrop-filter: invert(100%);
@@ -76,6 +76,6 @@ export const FocusImage = styled(Box)`
     left: 0;
     z-index: 100;
     margin: auto;
-    width: ${100 / 3}vw;
-    height: ${100 / 3}vw;
+    width: ${40}vw;
+    height: ${40}vw;
 `
