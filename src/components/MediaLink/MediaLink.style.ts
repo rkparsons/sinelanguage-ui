9ac8ca626e9d0488f044e7a0817b8ef7c3b2a453
Richard Parsons <parsons.rk@gmail.com>
2020-04-24 18:06:34 +0200

    Link hover effect

--- src/components/MediaLink/MediaLink.style.ts
+++ src/components/MediaLink/MediaLink.style.ts
@@ -1,12 +1,19 @@
-import { Link } from 'gatsby'
 import styled from 'styled-components'
 
-export const MediaLink = styled(Link)`
-    opacity: 1;
+export const MediaLink = styled.div`
+    position: relative;
+`
+
+export const Overlay = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: white;
+    opacity: 0;
 
     &:hover {
-        * * {
-            opacity: 0.6;
-        }
+        opacity: 0.4;
     }
 `
--- src/components/MediaLink/MediaLink.view.tsx
+++ src/components/MediaLink/MediaLink.view.tsx
@@ -1,10 +1,18 @@
+import { MediaLink, Overlay } from './MediaLink.style'
 import React, { ReactNode } from 'react'
 
-import { MediaLink } from './MediaLink.style'
+import { Link } from 'gatsby'
 
 type ViewProps = {
     url: string
     children: ReactNode
 }
 
-export default ({ url, children }: ViewProps) => <MediaLink to={url}>{children}</MediaLink>
+export default ({ url, children }: ViewProps) => (
+    <Link to={url}>
+        <MediaLink>
+            {children}
+            <Overlay />
+        </MediaLink>
+    </Link>
+)
