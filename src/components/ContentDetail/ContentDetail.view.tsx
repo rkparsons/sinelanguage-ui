2cf54f11bc599b4c1abb915c79584f5c6d682bab
Richard Parsons <parsons.rk@gmail.com>
2020-04-27 17:00:12 +0200

    Remove unused imports

--- src/components/ContentDetail/ContentDetail.view.tsx
+++ src/components/ContentDetail/ContentDetail.view.tsx
@@ -1,37 +1,27 @@
 import { Detail, Image, Info } from './ContentDetail.style'
-import { Grid, Typography } from '@material-ui/core'
 
 import { ContentModel } from '~/models'
-import Head from '~/components/Head'
-import IconButton from '~/components/IconButton'
-import { PlayArrow } from '@material-ui/icons'
-import { Podcast } from '~/cms/types'
+import { Grid } from '@material-ui/core'
 import React from 'react'
-import { SelectedMediaContext } from '~/contexts/selectedMediaContext'
-import SquareImage from '~/components/SquareImage'
 
 type ViewProps = {
     model: ContentModel
 }
 
 export default ({ model }: ViewProps) => (
-    <SelectedMediaContext.Consumer>
-        {({ setSelectedMedia }) => (
-            <>
-                {model.getSEOComponent()}
-                <Detail container>
-                    <Grid item xs={6}>
-                        <Image container justify="center" alignItems="center">
-                            <Grid item xs={7}>
-                                {model.getDashboardComponent()}
-                            </Grid>
-                        </Image>
+    <>
+        {model.getSEOComponent()}
+        <Detail container>
+            <Grid item xs={6}>
+                <Image container justify="center" alignItems="center">
+                    <Grid item xs={7}>
+                        {model.getDashboardComponent()}
                     </Grid>
-                    <Grid item xs={6}>
-                        <Info>{model.getDetailComponent()}</Info>
-                    </Grid>
-                </Detail>
-            </>
-        )}
-    </SelectedMediaContext.Consumer>
+                </Image>
+            </Grid>
+            <Grid item xs={6}>
+                <Info>{model.getDetailComponent()}</Info>
+            </Grid>
+        </Detail>
+    </>
 )
