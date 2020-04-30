71a726e19f226ad4141f3ebf951ac417dad6dbaf
Richard Parsons <parsons.rk@gmail.com>
2020-04-30 16:33:35 +0200

    Add date to event page

--- src/components/EventDetail/EventDetail.view.tsx
+++ src/components/EventDetail/EventDetail.view.tsx
@@ -1,11 +1,17 @@
 import { Event } from '~/cms/types'
 import React from 'react'
 import { Typography } from '@material-ui/core'
+import moment from 'moment'
 
 type ViewProps = {
     event: Event
 }
 
 export default ({ event }: ViewProps) => (
-    <Typography variant="h3">{event.title.toUpperCase()}</Typography>
+    <>
+        <Typography variant="h3">{event.title.toUpperCase()}</Typography>
+        <Typography variant="h3">
+            {moment(event.date).format(`ddd, DD MMM YYYY`).toUpperCase()}
+        </Typography>
+    </>
 )
