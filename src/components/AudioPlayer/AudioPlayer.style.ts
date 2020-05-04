70368693eb5ceb3567e2f8e23ac808b5605fc1c2
Richard Parsons <parsons.rk@gmail.com>
2020-05-04 18:41:42 +0200

    Slide player in

--- src/components/AudioPlayer/AudioPlayer.style.ts
+++ src/components/AudioPlayer/AudioPlayer.style.ts
@@ -2,6 +2,16 @@ import { marginTop } from '~/styles/sizes'
 import styled from 'styled-components'
 
 export const AudioPlayer = styled.div<{ isMinimised: boolean }>`
+    @keyframes slidein {
+        from {
+            bottom: -200px;
+        }
+
+        to {
+            bottom: 0;
+        }
+    }
+
     position: fixed;
     display: block;
     z-index: 1000;
@@ -9,6 +19,8 @@ export const AudioPlayer = styled.div<{ isMinimised: boolean }>`
     bottom: ${({ isMinimised }) => (isMinimised ? -100 : 0)}px;
     width: 100%;
     transition: bottom 0.1s ease;
+    animation-name: slidein;
+    animation-duration: 0.2s;
 `
 
 export const PlayerBody = styled.div`
