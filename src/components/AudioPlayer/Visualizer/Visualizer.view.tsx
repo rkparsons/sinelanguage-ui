2d518d5f7c74d960d9e5ea5649ba9cef9894847f
Richard Parsons <parsons.rk@gmail.com>
2020-05-06 14:15:49 +0200

    Animate entry of visualisation

--- src/components/AudioPlayer/Visualizer/Visualizer.view.tsx
+++ src/components/AudioPlayer/Visualizer/Visualizer.view.tsx
@@ -1,3 +1,4 @@
+import { Box, Zoom } from '@material-ui/core'
 import { Line, LineChart, ResponsiveContainer, YAxis } from 'recharts'
 
 import React from 'react'
@@ -9,19 +10,21 @@ type VisualiserProps = {
 }
 
 export default ({ audioData, isVisible }: VisualiserProps) => (
-    <Visualisation isVisible={isVisible}>
-        <ResponsiveContainer width="100%" height="100%">
-            <LineChart
-                data={Array.from(audioData).map((value, index) => ({
-                    x: index,
-                    y: value - 128,
-                    y2: 128 - value,
-                }))}
-            >
-                <YAxis type="number" domain={[-128, 128]} hide />
-                <Line type="monotone" dataKey="y2" stroke="#f7aec2" strokeWidth={2.5} />
-                <Line type="monotone" dataKey="y" stroke={'#a1cbdd'} strokeWidth={2.5} />
-            </LineChart>
-        </ResponsiveContainer>
-    </Visualisation>
+    <Zoom in={true} timeout={1500}>
+        <Visualisation isVisible={isVisible}>
+            <ResponsiveContainer width="100%" height="100%">
+                <LineChart
+                    data={Array.from(audioData).map((value, index) => ({
+                        x: index,
+                        y: value - 128,
+                        y2: 128 - value,
+                    }))}
+                >
+                    <YAxis type="number" domain={[-128, 128]} hide />
+                    <Line type="monotone" dataKey="y2" stroke="#f7aec2" strokeWidth={2.5} />
+                    <Line type="monotone" dataKey="y" stroke={'#a1cbdd'} strokeWidth={2.5} />
+                </LineChart>
+            </ResponsiveContainer>
+        </Visualisation>
+    </Zoom>
 )
