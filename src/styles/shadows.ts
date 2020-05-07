7c5e38e67ece954f8739108dce4b868aa6a628ef
Richard Parsons <parsons.rk@gmail.com>
2020-05-07 12:02:38 +0200

    Reduce shadow effect

--- src/styles/shadows.ts
+++ src/styles/shadows.ts
@@ -1,2 +1,6 @@
-export const darkShadow = `1px 2px 4px #000000`
-export const lightShadow = `1px 2px 4px #ffffff`
+const horizontal = 1,
+    vertical = 1,
+    blur = 2
+
+export const darkShadow = `${horizontal}px ${vertical}px ${blur}px #000000`
+export const lightShadow = `${horizontal}px ${vertical}px ${blur}px #ffffff`
