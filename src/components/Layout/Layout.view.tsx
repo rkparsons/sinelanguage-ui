285092390b24513cdecf6e9b85fc5cf58764a031
Richard Parsons <parsons.rk@gmail.com>
2020-05-11 16:21:44 +0200

    Fix ssr location

--- src/components/Layout/Layout.view.tsx
+++ src/components/Layout/Layout.view.tsx
@@ -1,13 +1,13 @@
 import 'font-awesome/css/font-awesome.css'
 
 import { AppContainer, GlobalStyle } from './Layout.style'
-import { Artist, Event, Podcast, Release, Track, Video } from '~/cms/types'
 import React, { ReactNode, useState } from 'react'
 
 import AudioPlayer from '~/components/AudioPlayer'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Dashboard from '~/components/Dashboard'
 import Footer from '~/components/Footer'
+import { Location } from '@reach/router'
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
 import Navigation from '~/components/Navigation'
 import { ThemeProvider as ScThemeProvider } from 'styled-components'
@@ -19,7 +19,6 @@ type ViewProps = {
     isDarkMode: boolean
     setIsDarkMode: (isDarkMode: boolean) => void
     children: ReactNode
-    location: Location
 }
 
 export default ({ isDarkMode, setIsDarkMode, children }: ViewProps) => {
@@ -33,7 +32,9 @@ export default ({ isDarkMode, setIsDarkMode, children }: ViewProps) => {
                         <CssBaseline />
                         <GlobalStyle />
                         <AppContainer>
-                            <Navigation location={location} />
+                            <Location>
+                                {({ location }) => <Navigation location={location} />}
+                            </Location>
                             <main>
                                 {children}
                                 <Dashboard />
