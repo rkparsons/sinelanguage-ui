e8cc2d42da022405f594593e31936cb9df89b5d4
Richard Parsons <parsons.rk@gmail.com>
2020-05-13 14:16:50 +0200

    Make top padding uniform

--- src/components/Menu/Menu.style.ts
+++ src/components/Menu/Menu.style.ts
@@ -1,10 +1,10 @@
-import { contentOffsetSmall, marginSide } from '~/styles/sizes'
+import { contentOffset, marginSide } from '~/styles/sizes'
 
 import styled from 'styled-components'
 
 export const Menu = styled.div`
     ${({ theme }) => `
-        padding-top: ${theme.spacing(contentOffsetSmall)};
+        padding-top: ${theme.spacing(contentOffset)};
         padding-left: ${theme.spacing(marginSide)};
         padding-right: ${theme.spacing(marginSide)};
     `}
--- src/styles/sizes.ts
+++ src/styles/sizes.ts
@@ -4,7 +4,6 @@ import { GridSpacing } from '@material-ui/core'
 export const marginSide = 4
 export const marginTop = 4
 export const contentOffset = 40
-export const contentOffsetSmall = 32
 export const listRowPadding = 1
 
 export const rowPadding: Record<Breakpoint, GridSpacing> = {
