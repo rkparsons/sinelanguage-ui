65596f1ed599491721c0313f63b070c805d4b870
Richard Parsons <parsons.rk@gmail.com>
2020-05-13 12:00:22 +0200

    Remove highlight color on tap

--- src/components/Layout/Layout.style.ts
+++ src/components/Layout/Layout.style.ts
@@ -11,6 +11,7 @@ export const GlobalStyle = createGlobalStyle`
         ::-webkit-scrollbar {
             display: none;
         }
+        -webkit-tap-highlight-color:  rgba(255, 255, 255, 0); 
     }
 
     a {
