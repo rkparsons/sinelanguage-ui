1ff88a545a136fdab005a3a5417a89ac3f29d282
Richard Parsons <parsons.rk@gmail.com>
2020-05-16 11:46:02 +0200

    Add price to cart button

--- src/components/Navigation/Links/Links.view.tsx
+++ src/components/Navigation/Links/Links.view.tsx
@@ -38,8 +38,7 @@ export default ({ isMobile }: ViewProps) => (
                     <CartButton className="snipcart-checkout">
                         <Typography variant="h3">BAG</Typography>
                         <Typography variant="h3">
-                            <span className="snipcart-total-price">£0.00</span>(
-                            <span className="snipcart-items-count">0</span>)
+                            (<span className="snipcart-total-price" />)
                         </Typography>
                     </CartButton>
                     {/* <NavItem to={Route.BAG} title="BAG" partiallyActive={true} /> */}
