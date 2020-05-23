d23636dfc2663f771a669c4bf6df6a261bd09110
Richard Parsons <parsons.rk@gmail.com>
2020-05-23 12:47:21 +0200

    Hide cart title on mobile

--- src/components/Cart/Elements/Media.ts
+++ src/components/Cart/Elements/Media.ts
@@ -7,5 +7,5 @@ type ViewProps = {
 }
 
 export default ({ children, name, className }: ViewProps) => {
-    return React.createElement('media', { name, className, is: 'x3d' }, children)
+    return React.createElement('media', { name, className }, children)
 }
