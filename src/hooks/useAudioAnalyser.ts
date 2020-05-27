14aa98697b33b02d03bcab48738b8665c5233700
Richard Parsons <parsons.rk@gmail.com>
2020-05-27 10:34:40 +0200

    Fix build

--- src/hooks/useAudioAnalyser.ts
+++ src/hooks/useAudioAnalyser.ts
@@ -1,33 +1,36 @@
 import { RefObject, useEffect, useRef } from 'react'
 
 export default (audioRef: RefObject<HTMLAudioElement>) => {
-    const analyser = useRef<AnalyserNode>()
+    const audioAnalyser = useRef<AnalyserNode>()
+    const audioSource = useRef<MediaElementAudioSourceNode>()
+    const isWindow = typeof window !== `undefined`
+    const isAudioContext = isWindow && ('AudioContext' in window || 'webkitAudioContext' in window)
 
     useEffect(() => {
-        var AudioContext = window.AudioContext || (<any>window).webkitAudioContext || false
-
-        if (!audioRef.current || !AudioContext) {
-            return
+        if (audioRef.current && isAudioContext && !audioSource.current) {
+            const audioContext = new (window.AudioContext || (window as any).webkitAudioContext)()
+            audioAnalyser.current = audioContext.createAnalyser()
+            audioSource.current = audioContext.createMediaElementSource(audioRef.current)
+            audioSource.current.connect(audioAnalyser.current)
+            audioSource.current.connect(audioContext.destination)
         }
+    }, [audioRef.current, audioSource.current])
 
-        const audioContext = new AudioContext()
-        analyser.current = audioContext.createAnalyser()
-        const source = audioContext.createMediaElementSource(audioRef.current)
-        source.connect(analyser.current)
-        source.connect(audioContext.destination)
-
-        return () => {
-            analyser.current?.disconnect()
-            source.disconnect()
+    useEffect(() => {
+        return function dispose() {
+            audioAnalyser.current?.disconnect()
+            audioSource.current?.disconnect()
+            audioAnalyser.current = undefined
+            audioSource.current = undefined
         }
     }, [])
 
     function getAudioData() {
-        if (!analyser.current) {
+        if (!audioAnalyser.current) {
             return []
         } else {
-            const dataArray = new Uint8Array(analyser.current.frequencyBinCount)
-            analyser.current.getByteTimeDomainData(dataArray)
+            const dataArray = new Uint8Array(audioAnalyser.current.frequencyBinCount)
+            audioAnalyser.current.getByteTimeDomainData(dataArray)
             return Array.from(dataArray).map((y) => y - 128)
         }
     }
