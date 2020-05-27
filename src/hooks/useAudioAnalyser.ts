ccffeaf9e7abd01d1230c155657dcc385b77d346
Richard Parsons <parsons.rk@gmail.com>
2020-05-27 11:26:11 +0200

    Remove touch interaction check

--- src/hooks/useAudioAnalyser.ts
+++ src/hooks/useAudioAnalyser.ts
@@ -7,10 +7,16 @@ export default (audioRef: RefObject<HTMLAudioElement>) => {
     const isAudioContext = isWindow && ('AudioContext' in window || 'webkitAudioContext' in window)
 
     useEffect(() => {
-        window.addEventListener('keydown', initialiseAudioAnalyser)
-        window.addEventListener('mousedown', initialiseAudioAnalyser)
-        window.addEventListener('touchstart', initialiseAudioAnalyser)
+        if (audioRef.current && isAudioContext && !audioSource.current) {
+            const audioContext = new (window.AudioContext || (window as any).webkitAudioContext)()
+            audioAnalyser.current = audioContext.createAnalyser()
+            audioSource.current = audioContext.createMediaElementSource(audioRef.current)
+            audioSource.current.connect(audioAnalyser.current)
+            audioSource.current.connect(audioContext.destination)
+        }
+    }, [audioRef.current, audioSource.current])
 
+    useEffect(() => {
         return function dispose() {
             audioAnalyser.current?.disconnect()
             audioSource.current?.disconnect()
@@ -18,24 +24,6 @@ export default (audioRef: RefObject<HTMLAudioElement>) => {
             audioSource.current = undefined
         }
     }, [])
-
-    function initialiseAudioAnalyser() {
-        if (audioRef.current && isAudioContext && !audioSource.current) {
-            const audioContext = new (window.AudioContext || (window as any).webkitAudioContext)()
-            audioAnalyser.current = audioContext.createAnalyser()
-            audioSource.current = audioContext.createMediaElementSource(audioRef.current)
-            audioSource.current.connect(audioAnalyser.current)
-            audioSource.current.connect(audioContext.destination)
-
-            removeInteractionListeners()
-        }
-    }
-
-    function removeInteractionListeners() {
-        window.removeEventListener('keydown', initialiseAudioAnalyser)
-        window.removeEventListener('mousedown', initialiseAudioAnalyser)
-        window.removeEventListener('touchstart', initialiseAudioAnalyser)
-    }
 
     function getAudioData() {
         if (!audioAnalyser.current) {
