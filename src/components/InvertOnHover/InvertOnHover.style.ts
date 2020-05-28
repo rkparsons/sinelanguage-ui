7517f4ca91ea7063a017c28178d5b58ae933d114
Richard Parsons <parsons.rk@gmail.com>
2020-05-28 13:11:14 +0200

    Remove invert rows on desktop

--- src/components/InvertOnHover/InvertOnHover.style.ts
+++ src/components/InvertOnHover/InvertOnHover.style.ts
@@ -7,7 +7,12 @@ export const InvertOnHover = styled.div`
     &:hover {
         text-shadow: ${lightShadow};
         color: black;
-        background-color: rgba(255, 255, 255, 0);
-        backdrop-filter: invert(100%);
+
+        ${({ theme }) => `                
+            ${theme.breakpoints.down('sm')} {
+                background-color: rgba(255, 255, 255, 0);
+                backdrop-filter: invert(100%);
+            }
+    `}
     }
 `
