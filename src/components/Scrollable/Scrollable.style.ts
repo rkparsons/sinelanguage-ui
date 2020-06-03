15cb7cda029dc13235b835a24bc78a3018d02f1d
Richard Parsons <parsons.rk@gmail.com>
2020-06-03 17:43:44 +0200

    Add scrollable opacity

--- src/components/Scrollable/Scrollable.style.ts
+++ src/components/Scrollable/Scrollable.style.ts
@@ -2,16 +2,18 @@ import { marginSide } from '~/styles/sizes'
 import styled from 'styled-components'
 
 export const Scrollable = styled.div<{ isWithMargin: boolean }>`
-    ${({ theme, isWithMargin }) => `    
-        height: 100vh;
-        overflow-x: hidden;
-        overflow-y: auto;
-        -webkit-overflow-scrolling: touch;
+    height: 100vh;
+    overflow-x: hidden;
+    overflow-y: auto;
+    -webkit-overflow-scrolling: touch;
+    mask-image: linear-gradient(transparent 5%, rgba(0, 0, 0, 1) 20%);
+
+    ${({ theme, isWithMargin }) => `  
         margin: 0 ${theme.spacing(isWithMargin ? marginSide : 0)};      
                 
         ${theme.breakpoints.up('md')} {
             margin: 0;
             margin-right: ${theme.spacing(isWithMargin ? marginSide : 0)};
-        }
+        }        
     `}
 `
