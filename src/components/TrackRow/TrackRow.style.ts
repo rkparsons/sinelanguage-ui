048f6a781c38ae3c7c561770cf63bd44413f2940
Richard Parsons <parsons.rk@gmail.com>
2020-06-03 12:53:15 +0200

    Style track rows

--- src/components/TrackRow/TrackRow.style.ts
+++ src/components/TrackRow/TrackRow.style.ts
@@ -1,14 +1,6 @@
 import { Box } from '@material-ui/core'
 import styled from 'styled-components'
 
-export const TrackRow = styled(Box)`
-    * {
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-    }
-`
-
 export const TrackNumber = styled.div`
     ${({ theme }) => `    
         width: ${theme.spacing(10)};
--- src/components/TrackRow/TrackRow.view.tsx
+++ src/components/TrackRow/TrackRow.view.tsx
@@ -1,10 +1,10 @@
 import { Box, Grid, Typography } from '@material-ui/core'
 import { Release, Track } from '~/cms/types'
-import { TrackNumber, TrackRow } from './TrackRow.style'
 
 import ContentPlayButton from '~/components/ContentPlayButton'
 import ProductMenu from '~/components/ProductMenu'
 import React from 'react'
+import { TrackNumber } from './TrackRow.style'
 import { getDurationTimestamp } from '~/utils/date'
 
 type ViewProps = {
@@ -18,47 +18,34 @@ export default ({ release, track, index }: ViewProps) => {
     const allProductDownloadsAvailable = products && products.every((product) => product.fileGUID)
 
     return (
-        <TrackRow display="flex">
+        <Box display="flex">
             <Box>
                 <TrackNumber>
                     <Typography variant="h3">{index + 1}</Typography>
                 </TrackNumber>
             </Box>
             <Box flexGrow={1}>
-                <Grid container key={index} justify="space-between">
-                    <Grid item xs={12} sm={8} md={9}>
-                        <Typography variant="h3">{title}</Typography>
-                    </Grid>
-                    <Grid item xs={3} sm={2} md={1}>
+                <Typography variant="h3">{title}</Typography>
+            </Box>
+            <Box>
+                <Box display="flex" width="100%" justifyContent="flex-end" alignItems="center">
+                    <Box paddingRight="20px">
                         <Typography variant="h3">
                             {getDurationTimestamp(metadata.duration)}
                         </Typography>
-                    </Grid>
-                    <Grid item xs={2}>
-                        <Box
-                            display="flex"
-                            width="100%"
-                            justifyContent="flex-end"
-                            alignItems="center"
-                        >
-                            <ContentPlayButton
-                                content={release}
-                                trackIndex={index}
-                                isLight={true}
-                            />
-                            {allProductDownloadsAvailable && (
-                                <ProductMenu
-                                    release={release}
-                                    products={products}
-                                    isLight={true}
-                                    isLarge={true}
-                                    indicateWhenInBag={true}
-                                />
-                            )}
-                        </Box>
-                    </Grid>
-                </Grid>
+                    </Box>
+                    <ContentPlayButton content={release} trackIndex={index} isLight={true} />
+                    {allProductDownloadsAvailable && (
+                        <ProductMenu
+                            release={release}
+                            products={products}
+                            isLight={true}
+                            isLarge={true}
+                            indicateWhenInBag={true}
+                        />
+                    )}
+                </Box>
             </Box>
-        </TrackRow>
+        </Box>
     )
 }
