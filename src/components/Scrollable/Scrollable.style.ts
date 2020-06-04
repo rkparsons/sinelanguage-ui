aab62e05ff18f21cfa602db3483f92b5a9c352c6
Richard Parsons <parsons.rk@gmail.com>
2020-06-04 18:47:49 +0200

    Fix fade breakpoints

--- src/components/Scrollable/Scrollable.style.ts
+++ src/components/Scrollable/Scrollable.style.ts
@@ -6,14 +6,19 @@ export const Scrollable = styled.div<{ isWithMargin: boolean }>`
     overflow-x: hidden;
     overflow-y: auto;
     -webkit-overflow-scrolling: touch;
-    mask-image: linear-gradient(transparent 5%, rgba(0, 0, 0, 1) 20%);
+    mask-image: linear-gradient(transparent 5%, rgba(0, 0, 0, 1) 15%);
 
     ${({ theme, isWithMargin }) => `  
         margin: 0 ${theme.spacing(isWithMargin ? marginSide : 0)};      
                 
         ${theme.breakpoints.up('md')} {
             margin: 0;
+            mask-image: linear-gradient(transparent 5%, rgba(0, 0, 0, 1) 20%);
             margin-right: ${theme.spacing(isWithMargin ? marginSide : 0)};
-        }        
+        }    
+
+        ${theme.breakpoints.up('xl')} {
+            mask-image: linear-gradient(transparent 5%, rgba(0, 0, 0, 1) 15%);
+        }       
     `}
 `
