6013624a7b049cdf9499890739dc178581747440
Richard Parsons <parsons.rk@gmail.com>
2020-06-04 17:00:27 +0200

    Remove user select style

--- src/components/Layout/Layout.style.ts
+++ src/components/Layout/Layout.style.ts
@@ -6,11 +6,13 @@ export const GlobalStyle = createGlobalStyle`
     * { 
         -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
         -webkit-tap-highlight-color: transparent;
-        -webkit-user-select: none;
+
+        /* -webkit-user-select: none;
         -khtml-user-select: none;
         -moz-user-select: none;
         -ms-user-select: none;
-        user-select: none;
+        user-select: none; */
+
         scrollbar-width: none;
         -ms-overflow-style: none;
         ::-webkit-scrollbar {
