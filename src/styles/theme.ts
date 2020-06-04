5f3dff9290aee3a60fdf6d8fec0607928d209642
Richard Parsons <parsons.rk@gmail.com>
2020-06-04 15:08:31 +0200

    Use font weight number

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -11,10 +11,9 @@ export default (isDarkMode = false) =>
             fontFamily: 'HelveticaNowText',
             allVariants: {
                 wordWrap: 'break-word',
-                fontWeight: 'normal',
                 lineHeight: 1.3,
             },
-            body1: { ...responsiveFont(15, 15, 13.5, 14, 15), fontWeight: 'bold' },
+            body1: { ...responsiveFont(15, 15, 13.5, 14, 15), fontWeight: 600 },
             h5: responsiveFont(18, 23, 18, 20, 21),
             h3: responsiveFont(20, 25, 20, 23, 25),
             h1: responsiveFont(22, 27, 22, 24, 25),
