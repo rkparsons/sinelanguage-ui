44c63ff65c20edafe95ce11be2b7e6f2a603bd5d
Richard Parsons <parsons.rk@gmail.com>
2020-06-04 15:54:57 +0200

    Fix contact styling

--- src/components/Contact/Email/Email.view.tsx
+++ src/components/Contact/Email/Email.view.tsx
@@ -1,4 +1,4 @@
-import { Grid, Hidden } from '@material-ui/core'
+import { Box, Hidden } from '@material-ui/core'
 
 import ExternalLink from '~/components/ExternalLink'
 import { MailOutline } from '@material-ui/icons'
@@ -12,20 +12,16 @@ type ViewProps = {
 
 export default ({ title, email }: ViewProps) => (
     <>
-        <Grid container>
-            <Hidden mdUp>
-                <Grid item>
-                    <ExternalLink href={`mailto:${email}`} title={title} icon={<MailOutline />} />
-                </Grid>
-            </Hidden>
-            <Hidden smDown>
-                <Grid item>
-                    <Title variant="h3">{title.toUpperCase()}</Title>
-                </Grid>
-                <Grid item>
-                    <ExternalLink href={`mailto:${email}`} title={email} />
-                </Grid>
-            </Hidden>
-        </Grid>
+        <Hidden mdUp>
+            <Box>
+                <ExternalLink href={`mailto:${email}`} title={title} icon={<MailOutline />} />
+            </Box>
+        </Hidden>
+        <Hidden smDown>
+            <Box display="flex">
+                <Title variant="h3">{title.toUpperCase()}</Title>
+                <ExternalLink href={`mailto:${email}`} title={email.toUpperCase()} />
+            </Box>
+        </Hidden>
     </>
 )
