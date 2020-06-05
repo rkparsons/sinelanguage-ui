da4d0d7aa8e770fdd40b1e23813ac8deaa5585e3
Richard Parsons <parsons.rk@gmail.com>
2020-06-05 16:50:53 +0200

    Use media link for playlist

--- src/components/MediaLink/MediaLink.view.tsx
+++ src/components/MediaLink/MediaLink.view.tsx
@@ -1,18 +1,28 @@
 import { MediaLink, Overlay } from './MediaLink.style'
 import React, { ReactNode } from 'react'
 
-import { Link } from 'gatsby'
+import { Link as GatsbyLink } from 'gatsby'
+import { Link as MuiLink } from '@material-ui/core'
 
 type ViewProps = {
     url: string
     children: ReactNode
+    isExternal?: boolean
 }
 
-export default ({ url, children }: ViewProps) => (
-    <Link to={url}>
-        <MediaLink>
-            {children}
-            <Overlay />
-        </MediaLink>
-    </Link>
-)
+export default ({ url, children, isExternal = false }: ViewProps) =>
+    isExternal ? (
+        <MuiLink href={url} target="_blank" rel="noopener">
+            <MediaLink>
+                {children}
+                <Overlay />
+            </MediaLink>
+        </MuiLink>
+    ) : (
+        <GatsbyLink to={url}>
+            <MediaLink>
+                {children}
+                <Overlay />
+            </MediaLink>
+        </GatsbyLink>
+    )
--- src/components/PlaylistCard/PlaylistCard.view.tsx
+++ src/components/PlaylistCard/PlaylistCard.view.tsx
@@ -1,17 +1,17 @@
-import { Link, Typography } from '@material-ui/core'
-
 import Column from '~/components/Column'
 import ContentCardDetail from '~/components/ContentCardDetail'
+import MediaLink from '~/components/MediaLink'
 import React from 'react'
 import SLR_Selections from './SLR_Selections.mp4'
 import TeaserVideo from '~/components/TeaserVideo'
+import { Typography } from '@material-ui/core'
 
 export default () => {
     return (
         <Column widthMultiplier={1}>
-            <Link href="http://slr.link/selections" target="_blank" rel="noopener">
+            <MediaLink url="http://slr.link/selections" isExternal={true}>
                 <TeaserVideo src={SLR_Selections} />
-            </Link>
+            </MediaLink>
             <ContentCardDetail>
                 <Typography variant="body1">
                     <i>SLR Selections</i>
