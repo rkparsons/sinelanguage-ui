fc1aba2159f01427411ca3ec7f26176ae7716874
Richard Parsons <parsons.rk@gmail.com>
2020-06-23 15:43:54 +0200

    Add comment

--- src/components/ProductMenu/ProductMenu.style.ts
+++ src/components/ProductMenu/ProductMenu.style.ts
@@ -5,6 +5,7 @@ import styled from 'styled-components'
 export const Popover = styled(MuiPopover)`
     .MuiPopover-paper {
         padding: ${({ theme }) => theme.spacing(2)};
+        /* todo: add shadow when ios rendering bug is fixed */
         box-shadow: ${isIOS ? 'none' : 'auto'};
         background-color: rgba(255, 255, 255, 0.8);
         @supports (backdrop-filter: blur(12px)) {
