0871c44181181628b3cfbe93db5e8ed977af2288
Richard Parsons <parsons.rk@gmail.com>
2020-06-23 09:21:30 +0200

    Fix mailchimp popup styling

--- src/components/MailchimpPopup/MailchimpPopup.style.ts
+++ src/components/MailchimpPopup/MailchimpPopup.style.ts
@@ -3,17 +3,31 @@ import { Paper, TextField } from '@material-ui/core'
 import { darkShadow } from '~/styles/shadows'
 import styled from 'styled-components'
 
-export const PopupContainer = styled(Paper)`
+export const AbsoluteContainer = styled.div`
     position: fixed;
     bottom: ${({ theme }) => theme.spacing(6)};
     right: ${({ theme }) => theme.spacing(6)};
     margin-left: ${({ theme }) => theme.spacing(6)};
-    z-index: 1000;
-    padding: ${({ theme }) => theme.spacing(3)};
+    z-index: 3000;
+`
+
+export const Content = styled(Paper)`
     border-radius: 8px;
     max-width: ${({ theme }) => theme.spacing(100)};
     height: ${({ theme }) => theme.spacing(50)};
-    box-shadow: none;
+    padding: ${({ theme }) => theme.spacing(3)};
+    background: none;
+`
+
+export const Blur = styled.div`
+    position: absolute;
+    z-index: -1;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 8px;
+
     background-color: rgba(255, 255, 255, 0.8);
     @supports (backdrop-filter: blur(12px)) {
         background-color: rgba(255, 255, 255, 0.15);
--- src/components/MailchimpPopup/MailchimpPopup.view.tsx
+++ src/components/MailchimpPopup/MailchimpPopup.view.tsx
@@ -1,5 +1,11 @@
+import {
+    AbsoluteContainer,
+    Blur,
+    Content,
+    EmailInput,
+    EmailInputContainer,
+} from './MailchimpPopup.style'
 import { Box, Slide, Typography } from '@material-ui/core'
-import { EmailInput, EmailInputContainer, PopupContainer } from './MailchimpPopup.style'
 import React, { useEffect, useState } from 'react'
 
 import IconButton from '~/components/IconButton'
@@ -43,71 +49,75 @@ export default () => {
             in={isActive}
             style={{ transitionDelay: isActive ? '5000ms' : '0ms' }}
         >
-            <PopupContainer elevation={3}>
-                <Box display="flex" flexDirection="column" height="100%">
-                    <Box display="flex" flexGrow={1}>
-                        <Typography variant="h5" gutterBottom>
-                            WANT TO HEAR FROM US ABOUT NEW RELEASES, MIXES AND LIVE EVENTS?
-                        </Typography>
-                        <Box marginLeft="20px">
-                            <IconButton
-                                label={<Typography variant="h5">{Unicode.CLOSE}</Typography>}
-                                isLight={false}
-                                onClick={dismissPopup}
-                                isInactiveShadow={false}
-                            />
+            <AbsoluteContainer>
+                <Content elevation={3}>
+                    <Box display="flex" flexDirection="column" height="100%">
+                        <Box display="flex" flexGrow={1}>
+                            <Typography variant="h5" gutterBottom>
+                                WANT TO HEAR FROM US ABOUT NEW RELEASES, MIXES AND LIVE EVENTS?
+                            </Typography>
+                            <Box marginLeft="20px">
+                                <IconButton
+                                    label={<Typography variant="h5">{Unicode.CLOSE}</Typography>}
+                                    isLight={false}
+                                    onClick={dismissPopup}
+                                    isInactiveShadow={false}
+                                />
+                            </Box>
+                        </Box>
+                        <Box>
+                            {!isSuccess && (
+                                <>
+                                    <Typography variant="h5">
+                                        <EmailInputContainer isInvalid={isInvalid}>
+                                            <EmailInput
+                                                inputRef={emailInput}
+                                                type="email"
+                                                value={email}
+                                                error={isInvalid}
+                                                onChange={onEmailChanged}
+                                                onKeyDown={onKeyDown}
+                                                spellCheck={false}
+                                                InputProps={{
+                                                    color: 'primary',
+                                                    style: {
+                                                        fontSize: 'inherit',
+                                                        fontWeight: 'inherit',
+                                                        lineHeight: 'inherit',
+                                                    },
+                                                }}
+                                                placeholder="EMAIL"
+                                            />
+                                        </EmailInputContainer>
+                                    </Typography>
+                                    <Box
+                                        display="flex"
+                                        justifyContent="flex-start"
+                                        width="100%"
+                                        paddingTop={2}
+                                    >
+                                        <IconButton
+                                            label={
+                                                <Typography variant="h5">
+                                                    JOIN THE MAILING LIST
+                                                </Typography>
+                                            }
+                                            isLight={false}
+                                            isInactiveShadow={false}
+                                            isDisabled={isInvalid || !email}
+                                            onClick={onSubmit}
+                                        />
+                                    </Box>
+                                </>
+                            )}
+                            {isSuccess && (
+                                <Typography variant="h5">THANKS FOR SUBSCRIBING!</Typography>
+                            )}
                         </Box>
                     </Box>
-                    <Box>
-                        {!isSuccess && (
-                            <>
-                                <Typography variant="h5">
-                                    <EmailInputContainer isInvalid={isInvalid}>
-                                        <EmailInput
-                                            inputRef={emailInput}
-                                            type="email"
-                                            value={email}
-                                            error={isInvalid}
-                                            onChange={onEmailChanged}
-                                            onKeyDown={onKeyDown}
-                                            spellCheck={false}
-                                            InputProps={{
-                                                color: 'primary',
-                                                style: {
-                                                    fontSize: 'inherit',
-                                                    fontWeight: 'inherit',
-                                                    lineHeight: 'inherit',
-                                                },
-                                            }}
-                                            placeholder="EMAIL"
-                                        />
-                                    </EmailInputContainer>
-                                </Typography>
-                                <Box
-                                    display="flex"
-                                    justifyContent="flex-start"
-                                    width="100%"
-                                    paddingTop={2}
-                                >
-                                    <IconButton
-                                        label={
-                                            <Typography variant="h5">
-                                                JOIN THE MAILING LIST
-                                            </Typography>
-                                        }
-                                        isLight={false}
-                                        isInactiveShadow={false}
-                                        isDisabled={isInvalid || !email}
-                                        onClick={onSubmit}
-                                    />
-                                </Box>
-                            </>
-                        )}
-                        {isSuccess && <Typography variant="h5">THANKS FOR SUBSCRIBING!</Typography>}
-                    </Box>
-                </Box>
-                <SVGBoxShadow radius={8} size={3} offset={1} opacity={0.3} />
-            </PopupContainer>
+                </Content>
+                <Blur />
+            </AbsoluteContainer>
         </Slide>
     )
 }
