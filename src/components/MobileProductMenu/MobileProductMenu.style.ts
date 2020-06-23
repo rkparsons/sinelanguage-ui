b8e98c2787023e94f033f76be7b25de72cca6627
Richard Parsons <parsons.rk@gmail.com>
2020-06-23 15:27:54 +0200

    Add product menu variations

--- src/components/MobileProductMenu/MobileProductMenu.style.ts
+++ src/components/MobileProductMenu/MobileProductMenu.style.ts
@@ -1,10 +1,29 @@
-import { Paper } from '@material-ui/core'
+import { Popover as MuiPopover, Paper } from '@material-ui/core'
+
 import styled from 'styled-components'
 
-export const Popup = styled.div`
-    all: revert;
+export const Popover = styled(MuiPopover)`
+    .MuiPopover-paper {
+        box-shadow: none;
+        background: none;
+    }
+`
+
+export const Shadow = styled(Paper)`
+    padding: ${({ theme }) => theme.spacing(2)};
+    background: none;
+`
+
+export const Blur = styled.div`
     position: absolute;
+    z-index: -1;
     top: 0;
     left: 0;
-    z-index: 3000;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255, 255, 255, 0.8);
+    @supports (backdrop-filter: blur(12px)) {
+        background-color: rgba(255, 255, 255, 0.1);
+        backdrop-filter: blur(12px);
+    }
 `
