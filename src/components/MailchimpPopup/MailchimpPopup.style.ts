703ab05efb98404b301a9e64f4795094b17c5a59
Richard Parsons <parsons.rk@gmail.com>
2020-06-23 09:29:40 +0200

    Remove unused import

--- src/components/MailchimpPopup/MailchimpPopup.style.ts
+++ src/components/MailchimpPopup/MailchimpPopup.style.ts
@@ -1,6 +1,5 @@
 import { Paper, TextField } from '@material-ui/core'
 
-import { darkShadow } from '~/styles/shadows'
 import styled from 'styled-components'
 
 export const AbsoluteContainer = styled.div`
