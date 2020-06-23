1511cf254b3543ec7469010567ad845ddc1e164f
Richard Parsons <parsons.rk@gmail.com>
2020-06-23 15:46:14 +0200

    Fix edge rendering

--- src/components/ProductMenu/ProductMenu.style.ts
+++ src/components/ProductMenu/ProductMenu.style.ts
@@ -1,12 +1,13 @@
+import { isEdge, isIOS } from 'react-device-detect'
+
 import { Popover as MuiPopover } from '@material-ui/core'
-import { isIOS } from 'react-device-detect'
 import styled from 'styled-components'
 
 export const Popover = styled(MuiPopover)`
     .MuiPopover-paper {
         padding: ${({ theme }) => theme.spacing(2)};
-        /* todo: add shadow when ios rendering bug is fixed */
-        box-shadow: ${isIOS ? 'none' : 'auto'};
+        /* todo: add shadow when ios/edge rendering bug is fixed */
+        box-shadow: ${isIOS || isEdge ? 'none' : 'auto'};
         background-color: rgba(255, 255, 255, 0.8);
         @supports (backdrop-filter: blur(12px)) {
             background-color: rgba(255, 255, 255, 0.1);
