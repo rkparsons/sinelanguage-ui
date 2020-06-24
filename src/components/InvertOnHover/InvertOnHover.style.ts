5cda95ed324a0e6202df6de8a874c69173b81442
Richard Parsons <parsons.rk@gmail.com>
2020-06-24 08:52:12 +0200

    Remove invert row on mobile

--- src/components/InvertOnHover/InvertOnHover.style.ts
+++ src/components/InvertOnHover/InvertOnHover.style.ts
@@ -7,12 +7,5 @@ export const InvertOnHover = styled.div`
     &:hover {
         text-shadow: ${lightShadow};
         color: black;
-
-        ${({ theme }) => `                
-            ${theme.breakpoints.down('sm')} {
-                background-color: rgba(255, 255, 255, 0);
-                backdrop-filter: invert(100%);
-            }
-    `}
     }
 `
