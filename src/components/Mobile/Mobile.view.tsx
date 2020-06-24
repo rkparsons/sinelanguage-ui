2f843ce23626a65e9afce3603169521d15a6f33a
Richard Parsons <parsons.rk@gmail.com>
2020-06-24 14:04:24 +0200

    Fix responsive layout

--- src/components/Mobile/Mobile.view.tsx
+++ src/components/Mobile/Mobile.view.tsx
@@ -1,9 +1,15 @@
-import { Hidden } from '@material-ui/core'
+import { Breakpoint } from '@material-ui/core/styles/createBreakpoints'
 import React from 'react'
 import { ReactNode } from 'react'
+import { withWidth } from '@material-ui/core'
 
 type ViewProps = {
     children: ReactNode
+    width: Breakpoint
 }
 
-export default ({ children }: ViewProps) => <Hidden mdUp>{children}</Hidden>
+export default withWidth()(({ children, width }: ViewProps) => {
+    const isMobile = ['xs', 'sm'].includes(width)
+
+    return <>{isMobile && children}</>
+})
