10d8a3f211698a66e7bb6c41b4fa03ac65aa5ff8
Richard Parsons <parsons.rk@gmail.com>
2020-07-22 13:19:13 +0200

    Fix contentful typings

--- env-variables.ts
+++ env-variables.ts
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv-safe'
+import dotenv from 'dotenv'
 
 export const environmentName =
     process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development'
@@ -8,6 +8,7 @@ console.log(`Using environment config: ${environmentName}`)
 dotenv.config({
     path: `.env.${environmentName}`,
 })
+console.log('spaceid', process.env.GATSBY_CTF_SPACE_ID)
 
 export const googleAnalyticsId = process.env['GATSBY_GOOGLE_ANALYTICS_ID']!
 
