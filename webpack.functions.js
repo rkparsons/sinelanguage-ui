c1bc138b1a6b65cddf0d06679735c505a86d83d9
Richard Parsons <parsons.rk@gmail.com>
2020-07-22 12:59:07 +0200

    Add dynamic env for lambdas

--- webpack.functions.js
+++ webpack.functions.js
@@ -1,10 +1,14 @@
 const Dotenv = require('dotenv-webpack')
 
+const environmentName = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development'
+
+console.log(`Using environment config: ${environmentName}`)
+
 // @see https://github.com/netlify/netlify-lambda#webpack-configuration
 module.exports = {
     plugins: [
         new Dotenv({
-            path: '.env.development',
+            path: `.env.${environmentName}`,
             safe: true,
         }),
     ],
