13e3d2bd0719241eb951b01b5240c48bf18cdfa6
Richard Parsons <parsons.rk@gmail.com>
2020-12-29 17:54:24 +0100

    Refactor artist

--- src/templates/artist.tsx
+++ src/templates/artist.tsx
@@ -3,11 +3,11 @@ import { Box, Grid } from '@material-ui/core'
 
 import ArtistDetail from '~/components/ArtistDetail'
 import ContentCardMedia from '~/components/ContentCardMedia'
+import Desktop from '~/components/Desktop'
 import Head from '~/components/Head'
+import Mobile from '~/components/Mobile'
 import Overlay from '~/components/Overlay'
 import React from 'react'
-import ResponsiveGrid from '~/components/ResponsiveGrid'
-import ResponsiveLineBreak from '~/components/ResponsiveLineBreak'
 import Scrollable from '~/components/Scrollable'
 import { detailImageSize } from '~/styles/sizes'
 import { graphql } from 'gatsby'
@@ -22,35 +22,55 @@ export default ({ data }: Props) => {
     const { title, description, image } = data.contentfulArtist
 
     return (
-        <Overlay>
+        <>
             <Head title={title} description={description.description} image={image.fluid.src} />
-            <Grid container>
-                <ResponsiveGrid item md={6} isDesktop={true}>
-                    <Box
-                        display="flex"
-                        width="100%"
-                        height="100%"
-                        justifyContent="center"
-                        alignItems="center"
-                    >
-                        <Box width={`${detailImageSize}vh`} maxWidth="80%">
-                            <ContentCardMedia content={data.contentfulArtist} />
-                        </Box>
-                    </Box>                    
-                </ResponsiveGrid>
+            <Overlay>
+                
+                <Mobile>
+                    <Grid container>
+                        <Grid item xs={12}>
+                            <Scrollable isWithMargin={true}>
+                                <ArtistDetail
+                                    artist={data.contentfulArtist}
+                                    releases={data.contentfulArtist.release || []}
+                                    videos={data.contentfulArtist.video || []}
+                                />
+                            </Scrollable>
+                        </Grid>
+                    </Grid> 
+                </Mobile>
 
-                <Grid item xs={12} md={6}>
-                    <Scrollable isWithMargin={true}>
-                        <ResponsiveLineBreak isDesktop={true} />
-                        <ArtistDetail
-                            artist={data.contentfulArtist}
-                            releases={data.contentfulArtist.release || []}
-                            videos={data.contentfulArtist.video || []}
-                        />
-                    </Scrollable>
-                </Grid>
-            </Grid>
-        </Overlay>
+                <Desktop>
+                    <Grid container>
+                        <Grid item md={6}>
+                            <Box
+                                display="flex"
+                                width="100%"
+                                height="100%"
+                                justifyContent="center"
+                                alignItems="center"
+                            >
+                                <Box width={`${detailImageSize}vh`} maxWidth="80%">
+                                    <ContentCardMedia content={data.contentfulArtist} />
+                                </Box>
+                            </Box>                    
+                        </Grid>
+
+                        <Grid item md={6}>
+                            <Scrollable isWithMargin={true}>
+                                <br />
+                                <ArtistDetail
+                                    artist={data.contentfulArtist}
+                                    releases={data.contentfulArtist.release || []}
+                                    videos={data.contentfulArtist.video || []}
+                                />
+                            </Scrollable>
+                        </Grid>
+                    </Grid> 
+                </Desktop>
+
+            </Overlay>
+        </>
     )
 }
 
