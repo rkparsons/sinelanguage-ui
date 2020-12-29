d035f44e33666c34280405891bfc6df421a7bf55
Richard Parsons <parsons.rk@gmail.com>
2020-12-29 17:10:29 +0100

    Remove unused component

--- src/components/ResponsiveBox/ResponsiveBox.style.ts
+++
@@ -1,7 +0,0 @@
-import { Box as MuiBox } from '@material-ui/core'
-import { responsiveStyles } from '~/styles/css'
-import styled from 'styled-components'
-
-export const Box = styled(MuiBox)`
-    ${responsiveStyles}
-`
