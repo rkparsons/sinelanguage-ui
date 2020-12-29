256d15fd142eb3b85fddeefba1910f50d414f0fd
Richard Parsons <parsons.rk@gmail.com>
2020-12-29 18:01:14 +0100

    Remove unused component

--- src/components/ResponsiveTypography/ResponsiveTypography.style.ts
+++
@@ -1,7 +0,0 @@
-import { Typography as MuiTypography } from '@material-ui/core'
-import { responsiveStyles } from '~/styles/css'
-import styled from 'styled-components'
-
-export const Typography = styled(MuiTypography)`
-    ${responsiveStyles}
-`
