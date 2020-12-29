22dafff641b6ffb2c0eb190296aec42dd3b03ff6
Richard Parsons <parsons.rk@gmail.com>
2020-12-29 17:51:50 +0100

    Refactor event

--- src/templates/event.tsx
+++ src/templates/event.tsx
@@ -1,13 +1,13 @@
 import { Box, Grid } from '@material-ui/core'
 
 import ContentCardMedia from '~/components/ContentCardMedia'
+import Desktop from '~/components/Desktop'
 import { Event } from '~/cms/types'
 import EventDetail from '~/components/EventDetail'
 import Head from '~/components/Head'
+import Mobile from '~/components/Mobile'
 import Overlay from '~/components/Overlay'
 import React from 'react'
-import ResponsiveGrid from '~/components/ResponsiveGrid'
-import ResponsiveLineBreak from '~/components/ResponsiveLineBreak'
 import Scrollable from '~/components/Scrollable'
 import { detailImageSize } from '~/styles/sizes'
 import { graphql } from 'gatsby'
@@ -25,27 +25,41 @@ export default ({ data }: Props) => {
         <>
             <Head title={title} description={description.description} image={image.fluid.src} />
             <Overlay>
-                <Grid container>
-                <ResponsiveGrid item md={6} isDesktop={true}>
-                    <Box
-                        display="flex"
-                        width="100%"
-                        height="100%"
-                        justifyContent="center"
-                        alignItems="center"
-                    >
-                        <Box width={`${detailImageSize}vh`} maxWidth="80%">
-                            <ContentCardMedia content={data.contentfulEvent} />
-                        </Box>
-                    </Box>
-                </ResponsiveGrid>
-                    <Grid item xs={12} md={6}>
-                        <Scrollable isWithMargin={true}>
-                            <ResponsiveLineBreak isDesktop={true} />
-                            <EventDetail event={data.contentfulEvent} />
-                        </Scrollable>
+
+                <Mobile>
+                    <Grid container>
+                        <Grid item xs={12}>
+                            <Scrollable isWithMargin={true}>
+                                <EventDetail event={data.contentfulEvent} />
+                            </Scrollable>
+                        </Grid>
                     </Grid>
-                </Grid>
+                </Mobile>
+
+                <Desktop>
+                    <Grid container>
+                        <Grid item md={6}>
+                            <Box
+                                display="flex"
+                                width="100%"
+                                height="100%"
+                                justifyContent="center"
+                                alignItems="center"
+                            >
+                                <Box width={`${detailImageSize}vh`} maxWidth="80%">
+                                    <ContentCardMedia content={data.contentfulEvent} />
+                                </Box>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <Scrollable isWithMargin={true}>
+                                <br />
+                                <EventDetail event={data.contentfulEvent} />
+                            </Scrollable>
+                        </Grid>
+                    </Grid>
+                </Desktop>
+
             </Overlay>
         </>
     )
